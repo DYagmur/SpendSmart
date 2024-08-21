e6faec5b623464a080251534170f6c41628f8855
DYagmur <duyguyagmurefe@gmail.com>
2024-08-21 23:22:58 +0300

    done

--- SpendSmart/Controllers/HomeController.cs
+++ SpendSmart/Controllers/HomeController.cs
@@ -8,9 +8,12 @@ namespace SpendSmart.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly SpendSmartDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, SpendSmartDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -20,16 +23,49 @@ namespace SpendSmart.Controllers
 
         public IActionResult Expenses()
         {
+            var allExpenses = _context.Expenses.ToList();
+
+            return View(allExpenses);
+        }
+
+        public IActionResult CreateEditExpense(int? id)
+        {
+            if(id != null)
+            {
+                //editing -> load an expense by id 
+                var expenseInDb = _context.Expenses.SingleOrDefault(x => x.Id == id);
+                return View(expenseInDb);
+            }
+
+
             return View();
         }
 
-        public IActionResult CreateEditExpense()
+        public IActionResult DeleteExpense(int id )
+
         {
-            return View();
+            var expenseInDb = _context.Expenses.SingleOrDefault(x => x.Id == id);
+            _context.Expenses.Remove(expenseInDb);
+            _context.SaveChanges();
+            return RedirectToAction("Expenses");
         }
 
         public IActionResult CreateEditExpenseForm(Expense model)
         {
+            if (model.Id == 0)
+            {
+                // create 
+                 _context.Expenses.Add(model);
+
+            }else
+            {
+                //editing
+                _context.Expenses.Update(model);
+
+            }
+
+            _context.SaveChanges();
+
             return RedirectToAction("Index");
         }
 
